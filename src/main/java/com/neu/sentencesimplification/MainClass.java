1c989d6c4c8848bf748b93593e58908dd4403bb7
Vishal Rajpal <vishalrajpal.apiit@gmail.com>
2016-08-02 00:59:24 -0700

    Created MainClass

---
+++ src/main/java/com/neu/sentencesimplification/MainClass.java
@@ -0,0 +1,10 @@
+package com.neu.sentencesimplification;
+
+/**
+ * MainClass: The entry point of the project.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
