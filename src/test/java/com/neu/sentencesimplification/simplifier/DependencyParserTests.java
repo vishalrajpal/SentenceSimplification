9d0d278d3059d9b6d67c8a2cdc26d479667f0147
Vishal Rajpal <vishalrajpal.apiit@gmail.com>
2016-08-06 14:50:25 -0700

    Meeting Notes & DependencyParserTests

--- src/test/java/com/neu/sentencesimplification/simplifier/DependencyParserTests.java
+++ src/test/java/com/neu/sentencesimplification/simplifier/DependencyParserTests.java
@@ -19,8 +19,8 @@ public class DependencyParserTests {
     private static final String SENTENCE_WITHOUT_CONJUNCTION_1_2 = "He picked peaches to stock up .";
 
     private static final String SENTENCE_WITH_CONJUNCTION_2 = "A restaurant served 9 pizzas during lunch and 6 during dinner today .";
-    private static final String SENTENCE_WITHOUT_CONJUNCTION_2_1 = "A restaurant served 9 pizzas during lunch .";
-    private static final String SENTENCE_WITHOUT_CONJUNCTION_2_2 = "A restaurant served 6 pizzas during dinner .";
+    private static final String SENTENCE_WITHOUT_CONJUNCTION_2_1 = "A restaurant served 9 pizzas during lunch today .";
+    private static final String SENTENCE_WITHOUT_CONJUNCTION_2_2 = "A restaurant served 6 pizzas during dinner today .";
 
     private static final String SENTENCE_WITH_CONUNCTION_3 = "Joan found 6 seashells and Jessica found 8 seashells on the beach .";
     private static final String SENTENCE_WITHOUT_CONJUNCTION_3_1 = "Joan found 6 seashells on the beach .";
