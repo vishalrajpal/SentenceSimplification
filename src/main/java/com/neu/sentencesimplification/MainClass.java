e9d4ec86680c06ebc584c1fa3dd060f732460cb6
vishalrajpal <vishalarajpal@gmail.com>
2016-09-18 19:36:37 -0700

    Automate run process

--- src/main/java/com/neu/sentencesimplification/MainClass.java
+++ src/main/java/com/neu/sentencesimplification/MainClass.java
@@ -2,6 +2,8 @@ package com.neu.sentencesimplification;
 
 import com.neu.sentencesimplification.stanfordcorenlp.DependenciesParser;
 
+import java.util.Scanner;
+
 /**
  * MainClass: The entry point of the project.
  */
@@ -10,6 +12,11 @@ public class MainClass {
     public static void main(String[] args) {
 
         //CrossValidation.createCrossValidationFolds("datasets/mawps/AdditionSubtraction.json", 5);
-        DependenciesParser.extractPartsOfSpeechFromDependencies("A restaurant served 9 pizzas during lunch and 6 during dinner today .");
+        //DependenciesParser.extractPartsOfSpeechFromDependencies("Jenny ran 0.6 mile and walked 0.4 mile . How much farther did Jenny run than walk ?");
+        final Scanner scanner = new Scanner(System.in);
+        String text;
+        while((text = scanner.nextLine()) != null) {
+            DependenciesParser.extractPartsOfSpeechFromDependencies(text);
+        }
     }
 }
